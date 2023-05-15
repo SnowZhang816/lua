0e7e1cabae6238731c53f3cc5698dda664ed7b3b
SnowZhang <SnowZhang@boyaa.com>
2023-05-15 15:12:50 +0800

    feat:运算符实现

--- go/api/consts.go
+++ go/api/consts.go
@@ -12,3 +12,26 @@ const (
 	LUA_TUSERDATA
 	LUA_TTHREAD
 )
+
+const (
+	LUA_OPADD = iota	//	+
+	LUA_OPSUB			//	-
+	LUA_OPMUl			//	*
+	LUA_OPMOD			//	%
+	LUA_OPPOW			//	^
+	LUA_OPDIV			//	/
+	LUA_OPIDIV			//	//
+	LUA_OPAND			//	&
+	LUA_OPOR			//	|
+	LUA_OPBXOR			//	~
+	LUA_OPSHL			//	<<
+	LUA_OPSHR			//	>>
+	LUA_OPUNM			//	-
+	LUA_OPBONT			//	~
+)
+
+const (
+	LUA_OPEQ	= iota	//	==
+	LUA_OPLT			//	<
+	LUA_OPLE			//	<=
+)		
---
+++ go/state/api_compare.go
@@ -0,0 +1,85 @@
+package state
+
+import "main/api"
+
+func (self *luaState) Compare(idx1, idx2 int, op api.CompareOP) bool {
+	a := self.stack.get(idx1)
+	b := self.stack.get(idx2)
+	switch op {
+	case api.LUA_OPEQ:		return _eq(a, b)
+	case api.LUA_OPLT:		return _lt(a, b)
+	case api.LUA_OPLE:		return _le(a, b)
+	default: panic("invalid compare op!")
+	}
+}
+
+func _eq(a, b luaValue) bool {
+	switch x := a.(type) {
+	case nil:
+		return b == nil
+	case bool:
+		y, ok := b.(bool)
+		return ok && x == y
+	case int64:
+		switch y := b.(type) {
+		case int64: 	return x == y
+		case float64:	return float64(x) == y
+		default:		return false
+		}
+	case float64:
+		switch y := b.(type) {
+			case int64: 	return x == float64(y)
+			case float64:	return x == y
+			default:		return false
+			}
+	case string:
+		y, ok := b.(string)
+		return ok && x == y
+	default:
+		return a == b
+	}
+}
+
+func _lt(a, b luaValue) bool {
+	switch x := a.(type) {
+	case int64:
+		switch y := b.(type) {
+		case int64: 	return x < y
+		case float64:	return float64(x) < y
+		}
+	case float64:
+		switch y := b.(type) {
+		case int64: 	return x < float64(y)
+		case float64:	return x < y
+		}
+	case string:
+		y, ok := b.(string)
+		if ok {
+			return x < y
+		}
+	}
+
+	panic("comparison error!")
+}
+
+func _le(a, b luaValue) bool {
+	switch x := a.(type) {
+	case int64:
+		switch y := b.(type) {
+		case int64: 	return x <= y
+		case float64:	return float64(x) <= y
+		}
+	case float64:
+		switch y := b.(type) {
+		case int64: 	return x <= float64(y)
+		case float64:	return x <= y
+		}
+	case string:
+		y, ok := b.(string)
+		if ok {
+			return x <= y
+		}
+	}
+
+	panic("comparison error!")
+}
